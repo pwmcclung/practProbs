ac17bb2f88fc80e51b7046cf8241bc36ab60823a
pwmcclung <pwmcclung@aol.com>
2025-02-13 06:44:41 -0700

    added weapons to git

---
+++ weapons.py
@@ -0,0 +1,15 @@
+def identify_weapon(character):
+    if character == 'Laval':
+        return 'Laval-Shado Valious'
+    elif character == 'Cragger':
+        return 'Cragger-Vengdualize'
+    elif character == 'Lagravis':
+        return 'Lagravis-Blazeprowlor'
+    elif character == 'Crominus':
+        return 'Crominus-Grandorius'
+    elif character == 'Tormak':
+        return 'Tormak-Tygafyre'
+    elif character == 'LiElla':
+        return 'LiElla-Roarburn'
+    else:
+        return 'Not a character'
